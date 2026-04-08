e2d44e8d08129565dae5baf1f1f068b119bd0819
matsumo-and <yh134.toisanda@gmail.com>
2026-04-09 03:25:56 +0900

    Format code

--- src/mappings/gm1.ts
+++ src/mappings/gm1.ts
@@ -1,105 +1,104 @@
-import { DrumMapping } from "./type";
-
+import { DrumMapping } from './type';
 
 export const DEFAULT_MAPPING: DrumMapping = {
   35: {
-    name: "Acoustic Bass Drum",
-    type: "Kick",
+    name: 'Acoustic Bass Drum',
+    type: 'Kick',
   },
   36: {
-    name: "Bass Drum 1",
-    type: "Kick",
+    name: 'Bass Drum 1',
+    type: 'Kick',
   },
   37: {
-    name: "Side Stick",
-    type: "SideStick",
+    name: 'Side Stick',
+    type: 'SideStick',
   },
   38: {
-    name: "Acoustic Snare",
-    type: "Snare",
+    name: 'Acoustic Snare',
+    type: 'Snare',
   },
   39: {
-    name: "Hand Clap",
-    type: "Snare",
+    name: 'Hand Clap',
+    type: 'Snare',
   },
   40: {
-    name: "Electric Snare",
-    type: "Snare",
+    name: 'Electric Snare',
+    type: 'Snare',
   },
   41: {
-    name: "Low Floor Tom",
-    type: "FloorTom",
+    name: 'Low Floor Tom',
+    type: 'FloorTom',
   },
   42: {
-    name: "Closed Hi-Hat",
-    type: "CloseHiHat",
+    name: 'Closed Hi-Hat',
+    type: 'CloseHiHat',
   },
   43: {
-    name: "High Floor Tom",
-    type: "FloorTom",
+    name: 'High Floor Tom',
+    type: 'FloorTom',
   },
   44: {
-    name: "Pedal Hi-Hat",
-    type: "CloseHiHat",
+    name: 'Pedal Hi-Hat',
+    type: 'CloseHiHat',
   },
   45: {
-    name: "Low Tom",
-    type: "LowTom",
+    name: 'Low Tom',
+    type: 'LowTom',
   },
   46: {
-    name: "Open Hi-Hat",
-    type: "OpenHiHat",
+    name: 'Open Hi-Hat',
+    type: 'OpenHiHat',
   },
   47: {
-    name: "Low-Mid Tom",
-    type: "LowTom",
+    name: 'Low-Mid Tom',
+    type: 'LowTom',
   },
   48: {
-    name: "Hi-Mid Tom",
-    type: "HighTom",
+    name: 'Hi-Mid Tom',
+    type: 'HighTom',
   },
   49: {
-    name: "Crash Cymbal 1",
-    type: "Crash",
+    name: 'Crash Cymbal 1',
+    type: 'Crash',
   },
   50: {
-    name: "High Tom",
-    type: "HighTom",
+    name: 'High Tom',
+    type: 'HighTom',
   },
   51: {
-    name: "Ride Cymbal 1",
-    type: "Ride",
+    name: 'Ride Cymbal 1',
+    type: 'Ride',
   },
   52: {
-    name: "Chinese Cymbal",
-    type: "China",
+    name: 'Chinese Cymbal',
+    type: 'China',
   },
   53: {
-    name: "Ride Bell",
-    type: "Ride",
+    name: 'Ride Bell',
+    type: 'Ride',
   },
   54: {
-    name: "Tambourine",
-    type: "Ride",
+    name: 'Tambourine',
+    type: 'Ride',
   },
   55: {
-    name: "Splash Cymbal",
-    type: "Splash",
+    name: 'Splash Cymbal',
+    type: 'Splash',
   },
   56: {
-    name: "Cowbell",
-    type: "Ride",
+    name: 'Cowbell',
+    type: 'Ride',
   },
   57: {
-    name: "Crash Cymbal 2",
-    type: "Crash",
+    name: 'Crash Cymbal 2',
+    type: 'Crash',
   },
   58: {
-    name: "Vibraslap",
-    type: "Crash",
+    name: 'Vibraslap',
+    type: 'Crash',
   },
   59: {
-    name: "Ride Cymbal 2",
-    type: "Ride",
+    name: 'Ride Cymbal 2',
+    type: 'Ride',
   },
 };
