f93a0cafcbd9bbd21bbc7a322069db0281f4d9ec
matdumo-and <yh134.toisanda@gmail.com>
2026-04-29 14:17:43 +0900

    Update AD2 mapping with full note-to-drum definitions
    Signed-off-by: matdumo-and <yh134.toisanda@gmail.com>

--- src/mappings/ad2.ts
+++ src/mappings/ad2.ts
@@ -4,28 +4,80 @@ import { DrumMapping } from './type';
  * Addictive Drums 2 custom drum map.
  */
 export const AD2_MAPPING: DrumMapping = {
-  36: {
-    name: 'Bass Drum 1',
-    type: 'Kick',
-  },
-  40: {
-    name: 'Crash Cymbal 1',
-    type: 'Crash',
-  },
-  41: {
-    name: 'Crash Cymbal 2',
-    type: 'Crash',
-  },
-  45: {
-    name: 'Acoustic Snare',
-    type: 'Snare',
-  },
-  65: {
-    name: 'High Floor Tom',
-    type: 'FloorTom',
-  },
-  69: {
-    name: 'High Tom',
-    type: 'HighTom',
-  },
+  36: { name: 'Kick', type: 'Kick' },
+
+  37: { name: 'Snare RimShot', type: 'Snare', articulation: 'rim' },
+  38: { name: 'Snare Open Hit', type: 'Snare', articulation: 'normal' },
+  39: { name: 'Snare RimShot (dbl)', type: 'Snare', articulation: 'rim' },
+  40: { name: 'Snare Open Hit (dbl)', type: 'Snare', articulation: 'normal' },
+  41: { name: 'Snare Shallow Rimshot', type: 'Snare', articulation: 'rim' },
+  42: { name: 'Snare SideStick', type: 'Snare', articulation: 'rim' },
+  43: { name: 'Snare Shallow Hit', type: 'Snare', articulation: 'normal' },
+  44: { name: 'Snare RimClick', type: 'Snare', articulation: 'rim' },
+
+  45: { name: 'Ride 1 Tip', type: 'Ride', articulation: 'normal' },
+  46: { name: 'Cymbal 1', type: 'Crash', articulation: 'normal' },
+
+  47: { name: 'Flexi 1 Hit A', type: 'Crash' },
+
+  48: { name: 'HH Foot Close', type: 'HiHat', articulation: 'pedal' },
+  49: { name: 'HH Closed 1 Tip', type: 'HiHat', articulation: 'close' },
+  50: { name: 'HH Closed 1 Shaft', type: 'HiHat', articulation: 'close' },
+  51: { name: 'HH Closed 2 Tip', type: 'HiHat', articulation: 'close' },
+  52: { name: 'HH Closed 2 Shaft', type: 'HiHat', articulation: 'close' },
+  53: { name: 'HH Closed Bell', type: 'HiHat', articulation: 'close' },
+  54: { name: 'HH Open A', type: 'HiHat', articulation: 'open' },
+  55: { name: 'HH Open B', type: 'HiHat', articulation: 'open' },
+  56: { name: 'HH Open C', type: 'HiHat', articulation: 'open' },
+  57: { name: 'HH Open D', type: 'HiHat', articulation: 'open' },
+  58: { name: 'HH Open Bell', type: 'HiHat', articulation: 'open' },
+  59: { name: 'HH Foot Splash', type: 'HiHat', articulation: 'pedal' },
+
+  60: { name: 'Ride 1 Tip', type: 'Ride', articulation: 'normal' },
+  61: { name: 'Ride 1 Bell', type: 'Ride', articulation: 'cup' },
+  62: { name: 'Ride 1 Shaft', type: 'Ride', articulation: 'normal' },
+  63: { name: 'Ride 1 Choke', type: 'Ride', articulation: 'normal' },
+
+  65: { name: 'Tom 4 Open Hit', type: 'FloorTom' },
+  66: { name: 'Tom 4 Rimshot', type: 'FloorTom' },
+  67: { name: 'Tom 3 Open Hit', type: 'LowTom' },
+  68: { name: 'Tom 3 Rimshot', type: 'LowTom' },
+  69: { name: 'Tom 2 Open Hit', type: 'LowTom' },
+  70: { name: 'Tom 2 Rimshot', type: 'LowTom' },
+  71: { name: 'Tom 1 Open Hit', type: 'HighTom' },
+  72: { name: 'Tom 1 Rimshot', type: 'HighTom' },
+
+  73: { name: 'Flexi 1 Hit B', type: 'Crash' },
+  74: { name: 'Flexi 1 Hit C', type: 'Crash' },
+  75: { name: 'Sticks', type: 'Snare', articulation: 'rim' },
+  76: { name: 'Flexi 1 Hit D', type: 'Crash' },
+
+  77: { name: 'Cymbal 1', type: 'Crash', articulation: 'normal' },
+  78: { name: 'Cymbal 1 Choke', type: 'Crash', articulation: 'normal' },
+  79: { name: 'Cymbal 2', type: 'Crash', articulation: 'normal' },
+  80: { name: 'Cymbal 2 Choke', type: 'Crash', articulation: 'normal' },
+  81: { name: 'Cymbal 3', type: 'Crash', articulation: 'normal' },
+  82: { name: 'Cymbal 3 Choke', type: 'Crash', articulation: 'normal' },
+
+  84: { name: 'Ride 2 Tip', type: 'Ride', articulation: 'normal' },
+  85: { name: 'Ride 2 Bell', type: 'Ride', articulation: 'cup' },
+  86: { name: 'Ride 2 Shaft', type: 'Ride', articulation: 'normal' },
+  87: { name: 'Ride 2 Choke', type: 'Ride', articulation: 'normal' },
+
+  89: { name: 'Cymbal 4', type: 'Crash', articulation: 'normal' },
+  90: { name: 'Cymbal 4 Choke', type: 'Crash', articulation: 'normal' },
+  91: { name: 'Cymbal 5', type: 'Crash', articulation: 'normal' },
+  92: { name: 'Cymbal 5 Choke', type: 'Crash', articulation: 'normal' },
+  93: { name: 'Cymbal 6', type: 'Crash', articulation: 'normal' },
+  94: { name: 'Cymbal 6 Choke', type: 'Crash', articulation: 'normal' },
+
+  96: { name: 'Flexi 2 Hit A', type: 'Crash' },
+  97: { name: 'Flexi 2 Hit B', type: 'Crash' },
+  98: { name: 'Flexi 2 Hit C', type: 'Crash' },
+  99: { name: 'Flexi 2 Hit D', type: 'Crash' },
+
+  101: { name: 'Flexi 3 Hit A', type: 'Crash' },
+  102: { name: 'Flexi 3 Hit B', type: 'Crash' },
+  103: { name: 'Flexi 3 Hit C', type: 'Crash' },
+  104: { name: 'Flexi 3 Hit D', type: 'Crash' },
 };
